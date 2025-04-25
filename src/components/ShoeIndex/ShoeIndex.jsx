f29474c832bce529df3d97e6a99973865bfc557b
Benoit Petitcollot <bpetitcollot@itnetwork.fr>
2025-04-25 14:55:42 +0200

    exercice 3 - shell

--- src/components/ShoeIndex/ShoeIndex.jsx
+++ src/components/ShoeIndex/ShoeIndex.jsx
@@ -42,13 +42,26 @@ const ShoeIndex = ({ sortId, setSortId }) => {
   );
 };
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+    display: flex;
+    gap: 32px;
+    flex-direction: row-reverse;
+    justify-content: flex-end;
+`;
 
-const LeftColumn = styled.div``;
+const LeftColumn = styled.div`
+    flex-shrink: 0;
+    padding-top: 10px;
+`;
 
-const MainColumn = styled.div``;
+const MainColumn = styled.div`
+    flex: 1;
+`;
 
-const Header = styled.header``;
+const Header = styled.header`
+    display: flex;
+    justify-content: space-between;
+`;
 
 const Title = styled.h2`
   font-size: 1.5rem;
